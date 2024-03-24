6819694cda8d9083f69c8c5aa14cec0735de8845
Алексей Красников <aleksey.mir79@gmail.com>
2024-03-25 01:23:59 +0500

    feat: error handler & custom exception

---
+++ Host/Extensions/ServiceExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Hangfire;
+using Hangfire.InMemory;
+using Host.Middleware;
+using System.Text.Json;
+
+namespace Host.Extensions
+{
+    public static class ServiceExtensions
+    {
+
+        public static void UseSwaggerExtension(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Centauri.Host");
+            });
+        }
+
+        public static void UseErrorHandlingMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+        }
+
+        public static void AddControllersExtension(this IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                })
+                ;
+        }
+
+        public static void AddBackgroundService(this IServiceCollection services)
+        {            
+            services.AddHangfire(config => config
+                .UseIgnoredAssemblyVersionTypeResolver()
+                .UseInMemoryStorage(new InMemoryStorageOptions
+                {
+                    MaxExpirationTime = TimeSpan.FromHours(6)
+                }));
+
+            services.AddHangfireServer(options => options.Queues = new[] { "critical", "default" });
+        }
+
+        //Configure CORS to allow any origin, header and method. 
+        //Change the CORS policy based on your requirements.
+        //More info see: https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-3.0
+
+        public static void AddCustomCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
+        }
+
+    }
+
+}
