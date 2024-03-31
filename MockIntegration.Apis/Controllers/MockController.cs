d4666726422f22fdc5170f9f84e09d106a8ed272
Алексей Красников <aleksey.mir79@gmail.com>
2024-04-01 02:28:04 +0500

    feat: mock api

---
+++ MockIntegration.Apis/Controllers/MockController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Host.Controllers
+{
+    public class MockController : Controller
+    {
+        // TODO добавить pagging
+        [ProducesResponseType(typeof(IReadOnlyCollection<Order>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response<Order>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("orders/")]
+        public async Task<IActionResult> GetAsync([FromQuery] string id)
+        {
+            _instrumentation.AddOrder();
+            //var response = await _aggregator.GetFlightsAsync(request, HttpContext.RequestAborted);
+            return Ok("hello" );
+        }
+    }
+}
