495dad2ce6b80a67abeb16f8023d03356d4482be
Алексей Красников <aleksey.mir79@gmail.com>
2024-03-31 23:41:37 +0500

    feat: metrics for monitoring

--- Host/Metrics/MetricsInstrumentation.cs
+++ Host/Metrics/MetricsInstrumentation.cs
@@ -2,26 +2,33 @@
 
 namespace Host.Metrics
 {
-    public class OrderInstrumentation
+    public class MetricsInstrumentation
     {       
         private Counter<int> OrdersCounter { get; }
+
+        private Counter<int> ExceptionCounter { get; }
+
         private Counter<int> TotalOrdersCounter { get; }
        
         private Histogram<double> OrdersPriceHistogram { get; }
       
 
-        public OrderInstrumentation(IMeterFactory meterFactory, IConfiguration configuration)
+        public MetricsInstrumentation(IMeterFactory meterFactory, IConfiguration configuration)
         {
             var meter = meterFactory.Create("orders.meter");
 
             OrdersCounter = meter.CreateCounter<int>("orders-counter", "order");
             TotalOrdersCounter = meter.CreateCounter<int>("total-orders", "orders");
-            OrdersPriceHistogram = meter.CreateHistogram<double>("orders-price", "euros", "delta price orders");
-            
+            ExceptionCounter = meter.CreateCounter<int>("orders-exception", "order");
+            OrdersPriceHistogram = meter.CreateHistogram<double>("orders-price", "euros", "delta price orders");  
+
         }
+
         public void AddOrder() => OrdersCounter.Add(1);
         public void AddTotalOrder() => TotalOrdersCounter.Add(1);
-      
+
+        public void AddException() => ExceptionCounter.Add(1);
+
         public void RecordOrderTotalPrice(double price) => OrdersPriceHistogram.Record(price);
         public void IncreaseTotalOrders(string city)
         {
@@ -30,7 +37,7 @@ namespace Host.Metrics
                 throw new ArgumentException($"'{nameof(city)}' cannot be null or whitespace.", nameof(city));
             }
 
-            TotalOrdersCounter.Add(1, KeyValuePair.Create<string, object>(key: "city", city));
+            TotalOrdersCounter.Add(1, KeyValuePair.Create<string, object?>(key: "city", city));
         }
     }
 }
