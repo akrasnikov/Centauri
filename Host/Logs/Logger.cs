cc8a3f3ec30afc197e098c0c9315c7d230156ad7
akrasnikov <aleksey.mir79@gmail.com>
2024-04-02 19:58:30 +0500

    feat: post-sharp logger

---
+++ Host/Logs/Logger.cs
@@ -0,0 +1,14 @@
+﻿using Castle.DynamicProxy;
+using System.Diagnostics;
+
+namespace Host.Logs
+{
+    public class DummyLogger : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Debug.WriteLine($"Calling {invocation.Method.Name} from Proxy");
+            invocation.Proceed();
+        }
+    }
+}
