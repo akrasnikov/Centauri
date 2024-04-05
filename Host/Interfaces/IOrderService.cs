ab9a80fffc693feb9b115485c7f0875d36257e39
akrasnikov <aleksey.mir79@gmail.com>
2024-04-05 19:10:44 +0500

    fixbug: lifetime for order service

--- Host/Interfaces/IOrderService.cs
+++ Host/Interfaces/IOrderService.cs
@@ -1,9 +1,10 @@
-﻿using Host.Models;
+﻿using Host.Common.Interfaces;
+using Host.Models;
 using Host.Requests;
 
 namespace Host.Interfaces
 {
-    public interface IOrderService 
+    public interface IOrderService : ITransientService
     {
         Task<OrdersModel> GetAsync(string id, CancellationToken cancellationToken = default);
         Task<OrdersModel> CreateAsync(OrderRequest request, CancellationToken cancellationToken = default);
