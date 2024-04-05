a5319f104a1228b5f79a109dcc6a8ff3848829af
akrasnikov <aleksey.mir79@gmail.com>
2024-04-05 19:09:55 +0500

    fixbug: yield for batch

--- Host/Extensions/BatchExtensions.cs
+++ Host/Extensions/BatchExtensions.cs
@@ -5,9 +5,9 @@
         public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> source, int batchSize)
         {
             while (source.Any())
-            {                
-                source = source.Skip(batchSize);
+            {
                 yield return source.Take(batchSize);
+                source = source.Skip(batchSize);               
             }
         }
     }
