a1f19b961e64b67cd47e67237889f082b796b90a
akrasnikov <aleksey.mir79@gmail.com>
2024-04-09 19:02:42 +0500

    feat: post sharp

---
+++ MockIntegration.Apis/PostSharp/LoggingAspect.cs
@@ -0,0 +1,27 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Serialization;
+using System.Drawing;
+
+namespace Host.Integration.PostSharp
+{
+    [PSerializable]
+
+    public class LoggingAspect : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"enter {args.Method.Name} with param: {string.Join(", ", args.Arguments)}");
+            Console.ForegroundColor = color;
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"exit from method {args.Method.Name} return value: {args.ReturnValue}");
+            Console.ForegroundColor = color;
+        }
+    }
+}
